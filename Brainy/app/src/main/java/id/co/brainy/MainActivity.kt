56f754aea9551e42b07c4a8feb2e994aafed8f33
Raqhib <raqhib.ataillah@gmail.com>
2025-05-03 23:13:28 +0800

    add SplashScreen & Logo Apps

--- Brainy/app/src/main/java/id/co/brainy/MainActivity.kt
+++ Brainy/app/src/main/java/id/co/brainy/MainActivity.kt
@@ -3,13 +3,36 @@ package id.co.brainy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import id.co.brainy.ui.theme.BrainyTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+        splashScreen.setKeepOnScreenCondition{
+            viewModel.isLoading.value
+        }
+
         setContent {
             BrainyTheme {
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+
+                ){
+                    Text(
+                        text = "Hello mas"
+                    )
+                }
 
             }
         }
