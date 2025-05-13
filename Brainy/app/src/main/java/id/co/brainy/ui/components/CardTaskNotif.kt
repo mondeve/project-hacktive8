5fea17b271721818f640f36b00b25195a4e71e94
ertreiter <guardian98x@gmail.com>
2025-05-13 11:32:21 +0800

    notif card

---
+++ Brainy/app/src/main/java/id/co/brainy/ui/components/CardTaskNotif.kt
@@ -0,0 +1,86 @@
+package id.co.brainy.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CardTaskNotif(
+    title: String,
+    category: String,
+    desc: String,
+    time: String
+) {
+    ElevatedCard(
+//        elevation = CardDefaults.cardElevation(
+//            defaultElevation = 6.dp
+//        ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondary.copy(
+                alpha = 0.6f
+            )
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+
+    ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color.White,
+                    )
+                )
+                Text(
+                    text = category,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(12.dp))
+                        .background(color = MaterialTheme.colorScheme.primary.copy(
+                        ))
+                        .padding(horizontal = 8.dp, vertical = 4.dp),
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        fontWeight = FontWeight.SemiBold,
+                        color = MaterialTheme.colorScheme.tertiary
+                    ),
+                )
+            }
+        }
+    }
+
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun CardTaskNotifPreview() {
+    CardTaskNotif(
+        title = "Task",
+        category = "Category",
+        time = "this time",
+        desc = "this is description"
+    )
+}
